ace1536fc69313d1297ee0fdb06039dabfc0a7d3
f <frt@frt.frt>
2018-07-13 17:45:38 +0300

    Add "delete" action for CommentaryController

--- modules/commentaries/controllers/CommentaryController.php
+++ modules/commentaries/controllers/CommentaryController.php
@@ -4,6 +4,7 @@ namespace app\modules\commentaries\controllers;
 
 use Yii;
 use yii\web\Controller;
+use yii\base\Module;
 
 use app\modules\commentaries\models\Commentary;
 
@@ -12,6 +13,24 @@ use app\modules\commentaries\models\Commentary;
  */
 class CommentaryController extends Controller
 {
+    /**
+     * Exception for all kinds of error while performing an action
+     * @var \yii\web\HttpException
+     */
+    private $invalidRequestException;
+
+    /**
+     * CommentaryController constructor.
+     * @param $id
+     * @param Module $module
+     * @param array $config
+     */
+    public function __construct($id, Module $module, array $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->invalidRequestException = new \yii\web\HttpException(400, 'Invalid request');
+    }
+
     public function behaviors()
     {
         return [
@@ -20,7 +39,7 @@ class CommentaryController extends Controller
     }
 
     /**
-     * Renders the index view for the module
+     * Returns JSON containing all commentaries
      * @return string
      */
     public function actionIndex()
@@ -31,7 +50,6 @@ class CommentaryController extends Controller
     /**
      * Creates new commentary
      * get params: content, user_id, created_at, position
-     *
      * @return \yii\web\Response
      * @throws \yii\web\HttpException
      */
@@ -41,7 +59,24 @@ class CommentaryController extends Controller
         if ($model->load(Yii::$app->request->get(), '') && $model->save()) {
             return $this->asJson($model);
         } else {
-            throw new \yii\web\HttpException(400, 'Invalid request params');
+            throw $this->invalidRequestException;
+        }
+    }
+
+    /**
+     * Deletes a commentary by id
+     * @param $id
+     * @return \yii\web\Response
+     * @throws \Throwable
+     */
+    public function actionDelete($id) {
+        $model = Commentary::find($id)->one();
+
+        try {
+            $model->delete();
+            return $this->asJson($model);
+        } catch (\Exception $e) {
+            throw $this->invalidRequestException;
         }
     }
 }
